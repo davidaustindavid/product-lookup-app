9bcf1dd7ad9764bf1f51a22f639d644841fe6c90
davidaustindavid <david@3heartblvd.com>
2025-04-10 15:03:31 -0500

    updates to fix error

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,26 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-      presets: ['babel-preset-expo'],
+      presets: [
+        ['babel-preset-expo', { jsxImportSource: 'nativewind' }]
+      ],
       plugins: [
         'nativewind/babel',
-        ['module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-          blocklist: null,
-          allowlist: null,
-          safe: false,
-          allowUndefined: true,
-        }],
+        [
+          'module:react-native-dotenv',
+          {
+            moduleName: '@env',
+            path: '.env',
+            blocklist: null,
+            allowlist: null,
+            safe: false,
+            allowUndefined: true,
+          },
+        ],
+        'react-native-reanimated/plugin',
       ],
     };
   };
-  
+  console.log('Babel config loaded');
+console.log('Babel config loaded');
+console.log('Babel config loaded');
